813e047f480a5f8189c75b0d1bb3f3ded61e0cd6
Molly <molly.gouletas@gmail.com>
2022-11-07 23:22:47 -0800

    Add ACH file structure logic

---
+++ files/file_structure.py
@@ -0,0 +1,226 @@
+"""Defines ACH file structure and how record types relate."""
+
+from math import ceil
+from typing import List, Optional, Tuple
+
+from record_types.addenda import AddendaRecordType
+from record_types.batch_control import BatchControlRecordType
+from record_types.batch_header import BatchHeaderRecordType
+from record_types.constants import TransactionCode, FILE_HEADER_BLOCKING_FACTOR, RECORD_SIZE
+from record_types.entry_detail import EntryDetailRecordType
+from record_types.file_control import FileControlRecordType
+from record_types.file_header import FileHeaderRecordType
+from record_types.record_type_base import RecordType
+
+
+class ACHFileContents:
+    """
+    Contains 1 FileHeaderRecordType, 1 FileControlRecord, and n ACHBatch.
+
+    Attributes:
+        file_header_record: FileHeaderRecordType
+        batches: List[ACHBatch]
+        [computed property] file_control_record: FileControlRecordType
+    """
+    def __init__(self, file_header_record: FileHeaderRecordType, batches: Optional[List['ACHBatch']] = None):
+        self.file_header_record = file_header_record
+        self.batches = batches or []
+        self._file_control_record = None
+        self._recalc_file_control = False
+
+    def get_rendered_line_list(self) -> List[str]:
+        record_types_list = [self.file_header_record.render_record_line()]
+        for batch in self.batches:
+            record_types_list.extend(batch.get_rendered_line_list())
+        record_types_list.append(self.file_control_record.render_record_line())
+        return record_types_list
+
+    def render_file_contents(self, line_break: str = '\n') -> str:
+        rendered_line_list = self.get_rendered_line_list()
+        file_contents = line_break.join(rendered_line_list)
+
+        block_orphan_count = len(rendered_line_list) % FILE_HEADER_BLOCKING_FACTOR
+        if block_orphan_count:
+            for _ in range(FILE_HEADER_BLOCKING_FACTOR - block_orphan_count):
+                file_contents += line_break + ('9' * RECORD_SIZE)
+
+        return file_contents
+
+    def set_file_header_record(self, file_header_record: FileHeaderRecordType) -> None:
+        self.file_header_record = file_header_record
+        if self._file_control_record:
+            self._recalc_file_control = True
+
+    def add_batch(self, batch: 'ACHBatch') -> None:
+        self.batches.append(batch)
+        if self._file_control_record:
+            self._recalc_file_control = True
+
+    def get_batches(self) -> List['ACHBatch']:
+        return self.batches
+
+    def remove_batch_by_index(self, index: int) -> 'ACHBatch':
+        batch = self.batches.pop(index)
+        if self._file_control_record:
+            self._recalc_file_control = True
+        return batch
+
+    @property
+    def file_control_record(self) -> FileControlRecordType:
+        if self._file_control_record and not self._recalc_file_control:
+            return self._file_control_record
+        self._file_control_record = self._compute_file_control_record()
+        return self._file_control_record
+
+    def _compute_file_control_record(self) -> FileControlRecordType:
+        debit_total, credit_total = self._compute_debit_and_credit_totals()
+        ent_add_count = self._compute_entry_and_addenda_count()
+        return FileControlRecordType(
+            batch_count=len(self.batches),
+            block_count=self._compute_block_count(entry_addenda_count=ent_add_count),
+            entry_and_addenda_count=ent_add_count,
+            entry_hash=self._compute_entry_hash(),
+            total_credit_amount=credit_total,
+            total_debit_amount=debit_total,
+        )
+
+    def _compute_entry_and_addenda_count(self) -> int:
+        return sum([int(x.batch_control_record.get_field_value('entry_and_addenda_count')) for x in self.batches])
+
+    def _compute_entry_hash(self) -> int:
+        return sum([int(x.batch_control_record.get_field_value('entry_hash')) for x in self.batches])
+
+    def _compute_line_count(self, entry_addenda_count: Optional[int] = None) -> int:
+        if entry_addenda_count is None:
+            entry_addenda_count = self._compute_entry_and_addenda_count()
+        return entry_addenda_count + (len(self.batches) * 2) + 2
+
+    def _compute_block_count(self, line_count: Optional[int] = None, entry_addenda_count: Optional[int] = None) -> int:
+        if line_count is None:
+            line_count = self._compute_line_count(entry_addenda_count)
+        return ceil(line_count / float(FILE_HEADER_BLOCKING_FACTOR))
+
+    def _compute_debit_and_credit_totals(self) -> Tuple[int, int]:
+        debit_sum, credit_sum = 0, 0
+        debit_sum = sum([int(x.batch_control_record.get_field_value('total_debit_amount')) for x in self.batches])
+        credit_sum = sum([int(x.batch_control_record.get_field_value('total_credit_amount')) for x in self.batches])
+        return debit_sum, credit_sum
+
+
+class ACHBatch:
+    """
+    Contains 1 BatchHeaderRecordType, 1 BatchControlRecordType, and n ACHTransactionEntry.
+
+    Attributes:
+        batch_header_record: BatchHeaderRecordType
+        transactions: List[ACHTransactionEntry]
+        [computed + cached property] batch_control_record: BatchControlRecordType
+    """
+    def __init__(self, batch_header_record: BatchHeaderRecordType, transactions: Optional[List['ACHTransactionEntry']]):
+        self.batch_header_record = batch_header_record
+        self.transactions = transactions or []
+        self._batch_control_record: BatchControlRecordType = None
+        self._recalc_batch_control = False
+    
+    def set_batch_header_record(self, batch_header_record: BatchHeaderRecordType) -> None:
+        self.batch_header_record = batch_header_record
+        if self._batch_control_record:
+            self._recalc_batch_control = True
+
+    def add_transaction(self, transaction: 'ACHTransactionEntry') -> None:
+        self.transactions.append(transaction)
+        if self._batch_control_record:
+            self._recalc_batch_control = True
+
+    def get_transactions(self) -> List['ACHTransactionEntry']:
+        return self.transactions
+    
+    def remove_transaction_by_index(self, index: int) -> 'ACHTransactionEntry':
+        transaction = self.transactions.pop(index)
+        if self._batch_control_record:
+            self._recalc_batch_control = True
+        return transaction
+
+    @property
+    def batch_control_record(self) -> BatchControlRecordType:
+        if self._batch_control_record and not self._recalc_batch_control:
+            return self._batch_control_record
+        self._batch_control_record = self._compute_batch_control_record()
+        return self._batch_control_record
+
+    def get_rendered_line_list(self) -> List[str]:
+        record_types_list = [self.batch_header_record.render_record_line()]
+        for tx in self.transactions:
+            record_types_list.extend(tx.get_rendered_line_list())
+        record_types_list.append(self.batch_control_record.render_record_line())
+        return record_types_list
+
+    def _compute_batch_control_record(self) -> BatchControlRecordType:
+        debit_total, credit_total = self._compute_debit_and_credit_totals()
+        return BatchControlRecordType(
+            entry_and_addenda_count=self._compute_entry_and_addenda_count(),
+            entry_hash=self._compute_entry_hash(),
+            total_debit_amount=debit_total,
+            total_credit_amount=credit_total,
+            company_identification=self.batch_header_record.get_field_value('company_identification'),
+            odfi_identification=self.batch_header_record.get_field_value('odfi_identification'),
+            batch_number=self.batch_header_record.get_field_value('batch_number'),
+        )
+
+    def _compute_entry_hash(self) -> int:
+        return sum([x.get_entry_hash_int() for x in self.transactions])
+
+    def _compute_entry_and_addenda_count(self) -> int:
+        return sum([x.get_entry_and_addenda_count() for x in self.transactions])
+
+    def _compute_debit_and_credit_totals(self) -> Tuple[int, int]:
+        debit_total = sum([x.get_debit_amount() for x in self.transactions])
+        credit_total = sum([x.get_credit_amount() for x in self.transactions])
+        return debit_total, credit_total
+
+
+class ACHTransactionEntry:
+    """
+    Contains 1 entry detail record and 0-n addendas associated with that record.
+
+    Attributes:
+        entry: EntryDetailRecordType
+        addendas: List[AddendaRecordType]
+    """
+    def __init__(
+        self,
+        entry: EntryDetailRecordType,
+        addendas: Optional[List[AddendaRecordType]] = None,
+    ):
+        self.entry = entry
+        self.addendas = addendas or []
+
+    def set_entry(self, entry: EntryDetailRecordType) -> None:
+        self.entry = entry
+
+    def add_addenda(self, addenda: AddendaRecordType) -> None:
+        self.addendas.append(addenda)
+
+    def remove_addenda_by_index(self, index: int) -> AddendaRecordType:
+        return self.addendas.pop(index)
+    
+    def get_entry_and_addenda_count(self) -> int:
+        return len(self.addendas) + 1
+    
+    def get_entry_hash_int(self) -> int:
+        return int(self.entry.get_field_value('rdfi_routing')[:8])
+
+    def get_transaction_code_enum(self) -> TransactionCode:
+        return TransactionCode(int(self.entry.get_field_value("transaction_code")))
+
+    def get_amount(self) -> int:
+        return int(self.entry.get_field_value("amount"))
+
+    def get_debit_amount(self) -> int:
+        return self.get_amount() if self.get_transaction_code_enum().is_debit() else 0
+
+    def get_credit_amount(self) -> int:
+        return self.get_amount() if self.get_transaction_code_enum().is_credit() else 0
+
+    def get_rendered_line_list(self) -> List[str]:
+        return [self.entry.render_record_line()] + [x.render_record_line() for x in self.addendas]
---
+++ tests/test_ach_file_structure.py
@@ -0,0 +1,98 @@
+"""Tests ACH file structure representation."""
+
+from unittest import TestCase
+
+from record_types.constants import RECORD_SIZE
+from files.file_structure import (
+    ACHBatch, ACHFileContents, ACHTransactionEntry,
+    AddendaRecordType, BatchHeaderRecordType, EntryDetailRecordType,
+    FileHeaderRecordType
+)
+
+test_file = """101 123456780 1234567801409020123A094101YOUR BANK              YOUR COMPANY                   
+5200YOUR COMPANY                        1234567890PPDPAYROLL         140903   1123456780000001
+62212345678011232132         0000001000               ALICE WANDERDUST        1123456780000001
+705HERE IS SOME ADDITIONAL INFORMATION                                             00010000001
+627123456780234234234        0000015000               BILLY HOLIDAY           0123456780000002
+622123232318123123123        0000001213               RACHEL WELCH            0123456780000003
+820000000400370145870000000150000000000022131234567890                         123456780000001
+9000001000001000000040037014587000000015000000000002213                                       
+9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+"""
+
+class TestACHFileContents(TestCase):
+    def test_ach_file_contents(self):
+        billy = EntryDetailRecordType(
+            transaction_code=27,
+            rdfi_routing=123456780,
+            rdfi_account_number=234234234,
+            amount=15000,
+            individual_name='BILLY HOLIDAY',
+            addenda_record_indicator=0,
+            trace_number=123456780000002,
+        )
+        file_contents = ACHFileContents(
+            FileHeaderRecordType(
+                destination_routing=123456780,
+                origin_routing=123456780,
+                destination_name='YOUR BANK',
+                origin_name='YOUR COMPANY',
+                file_creation_date='140902',
+                file_creation_time='0123',
+            ),
+            batches=[
+                ACHBatch(
+                    BatchHeaderRecordType(
+                        company_name='YOUR COMPANY',
+                        company_identification=1234567890,
+                        company_entry_description='PAYROLL',
+                        odfi_identification=112345678,
+                        batch_number=1,
+                        effective_entry_date='140903',
+                    ),
+                    transactions=[
+                        ACHTransactionEntry(
+                            EntryDetailRecordType(
+                                transaction_code=22,
+                                rdfi_routing=123456780,
+                                rdfi_account_number=11232132,
+                                amount=1000,
+                                individual_name='ALICE WANDERDUST',
+                                trace_number='123456780000001',
+                            ),
+                            addendas=[
+                                AddendaRecordType(
+                                    payment_related_information="HERE IS SOME ADDITIONAL INFORMATION",
+                                    entry_detail_sequence_number=1,
+                                    addenda_sequence_number=1,
+                                ),
+                            ],
+                        ),
+                        ACHTransactionEntry(
+                            billy,
+                        ),
+                        ACHTransactionEntry(
+                            EntryDetailRecordType(
+                                transaction_code=22,
+                                rdfi_routing=123232318,
+                                rdfi_account_number=123123123,
+                                amount=1213,
+                                individual_name='RACHEL WELCH',
+                                trace_odfi_identifier=12345678,
+                                trace_sequence_number=3,
+                                addenda_record_indicator=0,
+                            )
+                        )
+                    ],
+                ),
+            ],
+        )
+        print({x: y.value for x, y in file_contents.file_control_record.fields.items()})
+        file_contents_str = file_contents.render_file_contents()
+        print(file_contents_str)
+        file_lines = file_contents_str.splitlines()
+        test_file_lines = test_file.splitlines()
+        for i, line in enumerate(file_lines):
+            self.assertEqual(len(line), RECORD_SIZE)
+            self.assertEqual(line, test_file_lines[i])
