a624ae67d9ea3501965913b91c79f95a617cc9b5
Molly <molly.gouletas@gmail.com>
2022-11-12 13:51:44 -0800

    Update version

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ setup(
     name='ach-file',
     author='Molly Gouletas',
     author_email='molly.gouletas@gmail.com',
-    version='0.1.4.beta',
+    version='0.1.5.beta',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/freemish/ach-file',
     license='MIT License',
     description='Highly configurable and permissive library to generate ACH files',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
