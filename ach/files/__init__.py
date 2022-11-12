77bc457124918abec919fbf6d682a78b6108e48e
Molly <molly.gouletas@gmail.com>
2022-11-11 17:18:05 -0800

    Add docstrings for ach.files and ach.record_types

--- ach/files/__init__.py
+++ ach/files/__init__.py
@@ -1,3 +1,5 @@
+"""Defines file structure along with ways to translate to and from a flat ACH file."""
+
 from .file_builder import ACHFileBuilder, NoBatchForTransactionError
 from .file_parser import ACHFileContentsParser
 from .file_structure import ACHFileContents, ACHBatch, ACHTransactionEntry
